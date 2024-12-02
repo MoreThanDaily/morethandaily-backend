a0f595133e97536df81e604a4483b62c2443f376
이지수 <jisoolee031@gmail.com>
2024-12-02 09:32:19 +0900

    ADD. CORS

--- src/main/java/com/morethandaily/morethandaily/config/WebConfig.java
+++ src/main/java/com/morethandaily/morethandaily/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/") // 모든 엔드포인트에 대해
+        registry.addMapping("/**") // 모든 엔드포인트에 대해
                 .allowedOriginPatterns("*") // 와일드카드 패턴
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // 허용할 HTTP 메서드
     }
